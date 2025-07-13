1339453a37b65e83764aee57d20178adb951be5c
sparrow7559 <virajgupta2604@gmail.com>
2025-07-13 18:17:53 +0530

    Added login features

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 
 const Login = () => {
@@ -27,6 +27,27 @@ const Login = () => {
             placeholder="john@example.com"
             type="text"
           />
+          <Input
+            value={passsword}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            {" "}
+            Don't hvae an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signUp">
+              Sign Up
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
