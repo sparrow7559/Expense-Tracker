43a54e0a4124694df80e553f0e8f6fcf569caafe
sparrow7559 <virajgupta2604@gmail.com>
2025-07-14 16:38:04 +0530

    implement login functionality in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -38,6 +38,41 @@ exports.registerUser = async (req, res) => {
   }
 };
 
-exports.loginUser = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-// exports.registerUser = async (req, res) => {};
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    res.status(200).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering the user", error: err.message });
+  }
+};
+
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering the user", error: err.message });
+  }
+};
