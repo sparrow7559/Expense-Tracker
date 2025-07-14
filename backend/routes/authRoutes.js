ddaf01211d17b257fa6ab206936e4601782a8f5b
sparrow7559 <virajgupta2604@gmail.com>
2025-07-15 00:22:52 +0530

    add income and expense management functionality with routes and controllers

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -18,10 +18,10 @@ router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
-  const imageUrl = `${req.protocol}:??${req.get("host")}/uploads/${
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
     req.file.filename
   }`;
-  res.status(200).json({ message });
+  res.status(200).json({ imageUrl });
 });
 
 module.exports = router;
